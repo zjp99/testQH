32c2cf0cfeb530f9b635dce47a81fad3aa549e8d
DH <3238816982@qq.com>
2021-12-14 10:32:20 +0800

    tijiao

---
+++ mainwindow.cpp
@@ -0,0 +1,34 @@
+﻿#include "mainwindow.h"
+#include "ui_mainwindow.h"
+
+MainWindow::MainWindow(QWidget *parent)
+    : QMainWindow(parent)
+    , ui(new Ui::MainWindow)
+{
+    ui->setupUi(this);
+    m_pWindow = new QHalconWindow(ui->view,ui->view->width(),ui->view->height());
+    m_pWindow->GetHalconBuffer()->SetColored(12);
+    QString PicPath=QCoreApplication::applicationDirPath()+"/image.bmp";
+    std::string Str=PicPath.toStdString();
+    ReadImage(&ho_Image, Str.c_str());
+    GetImageSize(ho_Image, &hv_ImageWidth, &hv_ImageHeight);
+    m_pWindow->GetHalconBuffer()->SetPart(0,0,hv_ImageHeight[0].I()-1,hv_ImageWidth[0].I()-1);
+    m_pWindow->GetHalconBuffer()->DispObj(ho_Image);
+    m_pWindow->GetHalconBuffer()->FlushBuffer();
+    ui->tableWidget->setRowCount(4);
+    ui->tableWidget->setColumnCount(4);
+    for(int row=1;row<ui->tableWidget->rowCount();row++){
+        for(int column=1;column<ui->tableWidget->columnCount();column++){
+            ui->tableWidget->setItem(row,column,new QTableWidgetItem());
+
+          ui->tableWidget->item(row,column)->setTextAlignment(Qt::AlignCenter);
+        }
+    }
+    ui->tableWidget->show();
+}
+
+MainWindow::~MainWindow()
+{
+    delete ui;
+}
+
---
+++ mainwindow.h
@@ -0,0 +1,34 @@
+﻿#ifndef MAINWINDOW_H
+#define MAINWINDOW_H
+
+#include <QMainWindow>
+#include "HalconCpp.h"
+#include <QFileDialog>
+
+#  include "HDevEngineCpp.h"
+#  include "HDevThread.h"
+
+#include "qhalconwindow.h"
+
+using namespace HDevEngineCpp;
+using namespace HalconCpp;
+QT_BEGIN_NAMESPACE
+namespace Ui { class MainWindow; }
+QT_END_NAMESPACE
+
+class MainWindow : public QMainWindow
+{
+    Q_OBJECT
+
+public:
+    MainWindow(QWidget *parent = nullptr);
+    ~MainWindow();
+
+private:
+    Ui::MainWindow *ui;
+    QHalconWindow *m_pWindow;
+    HObject ho_Image;
+    HTuple hv_ImageWidth,hv_ImageHeight;
+
+};
+#endif // MAINWINDOW_H
